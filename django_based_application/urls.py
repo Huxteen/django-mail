d5c00c583b729190d23b5b32f4cd9cb6d4074a67
Augustine Okeke <hursteen@gmail.com>
2019-06-24 21:38:57 +0100

    added listview

--- django_based_application/urls.py
+++ django_based_application/urls.py
@@ -1,10 +1,11 @@
 from django.contrib import admin
 from django.urls import path, include
 from .views import home
-from mail.views import add
+from mail.views import (add, mail_list)
 
 urlpatterns = [
     path('admin/', admin.site.urls),
     path('', home, name='home'),
     path('add/', add, name='add'),
+    path('list/', mail_list, name='mail_list'),
 ]
--- mail/views.py
+++ mail/views.py
@@ -2,6 +2,7 @@ from django.shortcuts import render, redirect
 from django.http import HttpResponse
 from django.template import loader
 from mail.forms import CreateMailList
+from mail.models import MailList
 from django.http import JsonResponse, HttpResponse
 
 # View for the home Page
@@ -9,7 +10,9 @@ def add(request):
     if request.method == 'POST':
         form = CreateMailList(request.POST or None)
         data = {'message': 'Something went wrong with your form data',
-                'status': 'failed'}
+                'status': 'failed',
+                'error': form.errors,
+                }
         if form.is_valid():
             status = form.save()
             data = {
@@ -17,5 +20,10 @@ def add(request):
                 'status':'success', 
             }
         return JsonResponse(data)
-
     return render(request, 'mail/add.html')
+
+
+def mail_list(request):
+    mail_list = MailList.objects.all()
+    context = {'mail_list':mail_list}
+    return render(request, 'mail/list.html', context)
